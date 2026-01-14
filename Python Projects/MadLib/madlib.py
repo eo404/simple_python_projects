201ed8bc17386590d0e42f41614552231c7ea09c
eo404 <eldhox010@gmail.com>
2026-01-15 00:44:21 +0530

    madLib

---
+++ Python Projects/MadLib/madlib.py
@@ -0,0 +1,8 @@
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb (past tense): ")
+emotion = input("Enter an emotion: ")
+
+string = f"Yesterday, I found a {adjective} {noun} in my {place}.\nIt suddenly {verb}, and everyone around felt {emotion}."
+print(string)
